f77391e97165504bc246230d0fb8d25edce4bf1c
YuntaoHU <695250295@qq.com>
2024-04-25 16:50:37 +0800

    try stash pop --Code change:CubeScript
    add new code in 'Start'

--- Assets/Code/CubeScript.cs
+++ Assets/Code/CubeScript.cs
@@ -7,7 +7,7 @@ public class CubeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("cube script run!");
     }
     private void OnDisable()
     {
