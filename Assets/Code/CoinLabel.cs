e2083566fb3bd9f80d74641c316940a0a2f9fa1f
YuntaoHU <695250295@qq.com>
2024-05-06 18:16:13 +0800

    将原先coincontroller解耦分解出coinlabel
    coinlabel类-负责管理金币栏的显示相关

---
+++ Assets/Code/CoinLabel.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CoinLabel : MonoBehaviour
+{
+    int coinCnt;
+    TextMeshProUGUI coinTxt;
+    Coroutine anim;
+
+
+    private void Awake()
+    {
+        coinTxt = transform.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void OnEnable()
+    {
+        ShowCoinsCount();
+        CoinController.instance.onCoinChanged.AddListener(CoinsCountAnim);
+    }
+    private void OnDisable()
+    {
+        CoinController.instance.onCoinChanged.RemoveListener(CoinsCountAnim);
+        if (anim != null)
+        {
+            StopCoroutine(anim);
+        }
+    }
+
+    void ShowCoinsCount()
+    {
+        coinCnt = ApplicationModel.Coins;
+
+        if (coinTxt != null)
+            coinTxt.text = coinCnt.ToString();
+        else
+            Debug.Log("coin text is null!");
+    }
+
+    public void CoinsCountAnim()
+    {
+        if (anim != null)
+        {
+            StopCoroutine(anim);
+        }
+        anim = StartCoroutine(CoinsChangeAnim());
+    }
+    IEnumerator CoinsChangeAnim()
+    {
+        int curr = coinCnt;
+        int to = ApplicationModel.Coins;
+        float duration = 0.2f;
+        float time = 0f;
+
+        coinCnt = to;
+        if (coinTxt == null)
+        {
+            Debug.Log("coin text is null!");
+            yield break;
+        }
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            int cnt = curr + (int)((to - curr) * time / duration);
+            coinTxt.text = cnt.ToString();
+            yield return null;
+        }
+        coinTxt.text = to.ToString();
+    }
+}
--- Assets/Code/controller/CoinController.cs
+++ Assets/Code/controller/CoinController.cs
@@ -1,64 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinController : MonoBehaviour
 {
     public static CoinController instance;
 
-    public delegate void CoinControllerDelegate();
-    public static CoinControllerDelegate onCoinChange;
+    //public delegate void CoinControllerDelegate();
+    //public static CoinControllerDelegate onCoinChange;
+
+    public UnityEvent onCoinChanged;
 
     private void Awake()
     {
         if(instance == null)
             instance = this;
-
-        ShowCoinsCount();
-        onCoinChange += CoinsCountAnim;
-    }
-
-    void ShowCoinsCount()
-    {
-        transform.GetComponentInChildren<TextMeshProUGUI>().text = ApplicationModel.Coins.ToString(); ;
-    }
-
-    Coroutine anim;
-    public void CoinsCountAnim()
-    {
-        if(anim != null)
-        {
-            StopCoroutine(anim);
-        }
-        anim = StartCoroutine(CoinsChangeAnim());
-    }
-    IEnumerator CoinsChangeAnim()
-    {
-        int curr = int.Parse(transform.GetComponentInChildren<TextMeshProUGUI>().text);
-        int to = ApplicationModel.Coins;
-        float duration = 0.2f;
-        float time = 0f;
-
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-            int cnt = (int)((to - curr) * time / duration + curr);
-            transform.GetComponentInChildren<TextMeshProUGUI>().text = cnt.ToString();
-            yield return null;
-        }
-        transform.GetComponentInChildren<TextMeshProUGUI>().text = to.ToString();
     }
 
     public void AddCoins(int count)
     {
         ApplicationModel.Coins += count;
-        onCoinChange?.Invoke();
+        onCoinChanged?.Invoke();
     }
 
     public void ConsumeCoins(int count)
     {
         ApplicationModel.Coins -= count;
-        onCoinChange?.Invoke();
+        onCoinChanged?.Invoke();
     }
 }
